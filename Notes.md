6686536b4f9eee30495fed3faeb0bd051b813637
landon babay <landon.babay@caylent.com>
2024-01-03 00:01:00 -0500

    getting VPC for EKS cluster

---
+++ Notes.md
@@ -0,0 +1,9 @@
+# EKS storage options 
+- EBS CSI driver
+  - can mount these volumes as devices on our EC2 & container instances
+  - Provides block level storage volumes for use with EC2 & Container instances 
+  - EBS is well suited for database style applications that rely on random reads and writes, and to throughput intensive applications that perform long, continuous reads and writes
+
+- EFS CSI driver 
+- FSx for Luster CSI 
+
---
+++ infrastructure/vpc.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dhcp_options = true 
+  enable_nat_gateway = true
+
+  tags = { 
+    Environment = "dev"
+  }
+}
